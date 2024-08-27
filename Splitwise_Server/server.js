1f8b6c562f2a1e1a446009ee972d00373dd72cfd
Divyam Patel <pateldivyam26@gmail.com>
2024-08-27 10:48:20 +0530

    connected to cluster

--- Splitwise_Server/server.js
+++ Splitwise_Server/server.js
@@ -12,7 +12,11 @@ app.use(express.json());
 app.use(cors());
 
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri);
+// mongoose.connect(uri);
+
+mongoose.connect(uri)
+.then(() => console.log('Connected to MongoDB Atlas!'))
+.catch(err => console.error('Could not connect to MongoDB Atlas:', err));
 
 // User registration
 app.post('/register', async (req, res) => {
